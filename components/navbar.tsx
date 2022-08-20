df9934ab9ec714d2f076ea6225e03b81f5659d31
thakur-meg <meghnathakur2001@gmail.com>
2022-08-20 11:46:44 +0530

    nextauth config

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,23 +2,36 @@ import React from 'react'
 import Image from 'next/image'
 import { MenuIcon, ChevronDownIcon, HomeIcon, SearchIcon } from '@heroicons/react/solid'
 import { StarIcon, BellIcon, PlusIcon, ChatIcon } from '@heroicons/react/outline'
+import { signIn } from 'next-auth/react'
 
 function navbar() {
   return (
-    <div className='flex px-4 py-2 shadow-sm sticky top-0 z-50'>
+    <div className='flex px-4 py-2 shadow-sm sticky top-0 z-50 mr-5'>
         <div className='relative h-10 w-10 flex-shrink-0 cursor-pointer'>
             <Image src='/../public/img/ht-logo.png' layout='fill' objectFit='contain'/>
         </div>
-        <div className='flex items-center xl:min-w-[300px]'>
+        <div className='flex xl:min-w-[300px] text-blue-500 items-center '>
             <HomeIcon className='h-5 w-5'/>
             <p className='ml-2 hidden flex-1 lg:inline'>Home</p>
             <ChevronDownIcon className='h-5 w-5'/>
         </div>
-        <form className='flex flex-1 items-center space-x-2 rounded-sm border-gray-200 bg-gray-100 px-3 py-1'>
-            <SearchIcon  className='h-6 w-6 text-gray-400'/>
+        <form className='flex flex-1 items-center space-x-2 rounded-sm border-gray-200 bg-blue-100 px-3 py-1'>
+            <SearchIcon  className='h-6 w-6 text-blue-500'/>
             <input type='text' placeholder='Search' className='flex-1 bg-transparent outline-none'></input>
             <button type='submit' hidden/>
         </form>
+        <div className='flex text-blue-400 hidden lg:inline-flex items-center space-x-2 '>
+            <ChatIcon className='icon' />
+            <BellIcon className='icon' />
+            <PlusIcon className='icon' />
+        </div>
+        <div className='ml-5 flex items-center lg:hidden text-blue-500'>
+            <MenuIcon className='icon' />
+        </div>
+
+        <div className='hidden lg:flex items-center p-2 border-gray-100 cursor-pointer' onClick={() => signIn()}>
+            <p className='text-blue-500'>Sign In</p>
+        </div>
     </div>
   )
 }
