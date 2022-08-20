55102808b7ce607dd607519d556ae00d1f62ef7a
thakur-meg <meghnathakur2001@gmail.com>
2022-08-20 17:41:15 +0530

    connecting graphql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import Navbar from '../components/navbar'
+import { ApolloProvider } from '@apollo/client'
+import client from '../apollo-client'
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
+  <ApolloProvider client={client}>
   <SessionProvider session={session}>
-      <div className='h-screen overflow-y-scroll bg-blue-200'> 
+      <div className='h-screen overflow-y-scroll bg-blue-100'> 
         <Navbar />
         <Component {...pageProps} />
 
       </div>
       
   </SessionProvider>
+  </ApolloProvider>
   )
 }
 
